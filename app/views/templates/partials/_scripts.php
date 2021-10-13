d813c8cb4fa21e87b8228d5ba4c462e9a7c45a10
sabine-xuaitsep <sabinepestiaux@gmail.com>
2021-10-13 22:59:49 +0200

    Template fragmentation

---
+++ app/views/templates/partials/_scripts.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  ./app/views/templates/partials/_scripts.php
+*/
+?>
+
+<!-- All Javascript Plugins  -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/plugin.js"></script>
+
+<!-- Main Javascript File  -->
+<script type="text/javascript" src="js/scripts.js"></script>
